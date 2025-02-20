287c7d4c8fddf12c069697646878eb6ca6b43edb
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-02-20 21:22:12 +0700

    Add files via upload

---
+++ TelegramChatBot/TelegramChatBot/Pages/IPage.cs
@@ -0,0 +1,40 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramChatBot.Pages
+{
+    public interface IPage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TelegramBotClient BotClient { get; set; }
+
+        /// <summary>
+        /// Вывести начальную информацию страницы
+        /// </summary>
+        public void Open();
+
+        /// <summary>
+        /// Закрыть страницу 
+        /// </summary>
+        public void Close();
+
+        /// <summary>
+        /// Закрыть страницу с переходом на следующую
+        /// </summary>
+        /// <param name="page"></param>
+        public void Close(IPage page);
+
+        /// <summary>
+        /// Обработчик текстовых сообщений
+        /// </summary>
+        /// <param name="update"></param>
+        public Task OnMessageHandler(Update update);
+
+        /// <summary>
+        /// Обработчик inline команд
+        /// </summary>
+        /// <param name="update"></param>
+        public Task OnCallbackQueryHandler(Update update);
+    }
+}
