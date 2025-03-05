17dee5e164fb26ee17645518bb7542f91d4f81d6
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-03-05 16:52:34 +0700

    Add files via upload

---
+++ Sample_1 - telegram chat bot (Class BasePage)/TelegramChatBot/Pages/BasePage.cs
@@ -0,0 +1,62 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramChatBot.Pages
+{
+    public abstract class BasePage
+    {
+        protected int _id = 0;
+        protected string _name = "";
+        protected TelegramBotClient? _botClient = null;
+
+        public int Id => _id;
+        public string Name => _name;
+        public TelegramBotClient? BotClient => _botClient;
+
+        protected BasePage(int id, string name, TelegramBotClient client)
+        {
+            _id = id; 
+            _name = name;
+            _botClient = client;
+        }
+
+        protected BasePage(int id, TelegramBotClient client)
+        {
+            _id = id;
+            _botClient = client;
+        }
+
+        protected BasePage(TelegramBotClient client)
+        {
+            _botClient = client;
+        }
+
+        /// <summary>
+        /// Открыть страницу: вывести начальную информацию страницы
+        /// </summary>
+        public abstract void Open();
+
+        /// <summary>
+        /// Закрыть страницу: закрыть все необходимые связи
+        /// </summary>
+        public abstract void Close();
+
+        /// <summary>
+        /// Закрыть страницу с переходом на следующую
+        /// </summary>
+        /// <param name="page"></param>
+        public abstract void Close(BasePage page);
+
+        /// <summary>
+        /// Обработчик текстовых сообщений
+        /// </summary>
+        /// <param name="update"></param>
+        public abstract Task OnMessageHandler(Update update);
+
+        /// <summary>
+        /// Обработчик inline команд
+        /// </summary>
+        /// <param name="update"></param>
+        public abstract Task OnCallbackQueryHandler(Update update);
+    }
+}
