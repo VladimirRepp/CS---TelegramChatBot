f7f236933c50262bc742b0c04cb3db59507a9e52
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-12-02 20:12:14 +0700

    Update ButtonNames.cs

--- Sample_1 - user session/TelegramChatBot/TelegramChatBot/Global/ButtonNames.cs
+++ Sample_1 - user session/TelegramChatBot/TelegramChatBot/Global/ButtonNames.cs
@@ -3,22 +3,23 @@ namespace TelegramChatBot.Global
     public static class ButtonNames
     {
         // === Common === //
-        public static string START_NAME_BUTTON = "button_";
+        public readonly static string START_NAME_BUTTON = "button_";
 
         // === Button Names For LoginMainMenuCommands === //
-        public static string STUDENT_LOGIN = START_NAME_BUTTON + "student_login";
-        public static string TEACHER_LOGIN = START_NAME_BUTTON + "teacher_login";
+        public readonly static string STUDENT_LOGIN = START_NAME_BUTTON + "student_login";
+        public readonly static string TEACHER_LOGIN = START_NAME_BUTTON + "teacher_login";
 
         // === Button Names For StudentMainMenuCommands === //
-        public static string STUDENT_BUTTON_1 = START_NAME_BUTTON + "1_student";
-        public static string STUDENT_BUTTON_2 = START_NAME_BUTTON + "2_student";
-        public static string STUDENT_BUTTON_3 = START_NAME_BUTTON + "3_student";
-        public static string STUDENT_BACK = START_NAME_BUTTON + "student_back";
+        public readonly static string STUDENT_BUTTON_1 = START_NAME_BUTTON + "1_student";
+        public readonly static string STUDENT_BUTTON_2 = START_NAME_BUTTON + "2_student";
+        public readonly static string STUDENT_BUTTON_3 = START_NAME_BUTTON + "3_student";
+        public readonly static string STUDENT_BACK = START_NAME_BUTTON + "student_back";
 
         // === Button Names For StudentMainMenuCommands === //
-        public static string TEACHER_BUTTON_1 = START_NAME_BUTTON + "1_teacher";
-        public static string TEACHER_BUTTON_2 = START_NAME_BUTTON + "2_teacher";
-        public static string TEACHER_BUTTON_3 = START_NAME_BUTTON + "3_teacher";
-        public static string TEACHER_BACK = START_NAME_BUTTON + "teacher_back";
+        public readonly static string TEACHER_BUTTON_1 = START_NAME_BUTTON + "1_teacher";
+        public readonly static string TEACHER_BUTTON_2 = START_NAME_BUTTON + "2_teacher";
+        public readonly static string TEACHER_BUTTON_3 = START_NAME_BUTTON + "3_teacher";
+        public readonly static string TEACHER_BACK = START_NAME_BUTTON + "teacher_back";
     }
 }
+
